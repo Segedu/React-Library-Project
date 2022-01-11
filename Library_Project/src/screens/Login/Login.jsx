314c9942b5fcbdd425fc447f3d17f17ee52f053a
Segedu <seggedu20@gmail.com>
2022-01-11 18:23:01 +0200

    Login Component

--- Library_Project/src/screens/Login/Login.jsx
+++ Library_Project/src/screens/Login/Login.jsx
@@ -41,7 +41,7 @@ const Login = ({ setAuth }) => {
                 e.preventDefault(),
                     login()
             }}>
-                <input type="emil" placeholder="Enter Your Email" onChange={(e) => { setEmail(e.target.value) }} /><br></br>
+                <input type="email" placeholder="Enter Your Email" onChange={(e) => { setEmail(e.target.value) }} /><br></br>
                 <input type="password" placeholder="Enter Your Password" onChange={(e) => { setPassword(e.target.value) }} /><br></br>
                 <input type="submit" value="Log-In" />
             </form>
